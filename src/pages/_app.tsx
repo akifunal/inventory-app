eb9a2de5ce164d5cbaf1eac6da105448ecc92750
akif <akifunal05@gmail.com>
2022-07-14 01:23:39 +0300

    refactor: getbaseurl moved to constants

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-// src/pages/_app.tsx
 import { withTRPC } from '@trpc/next';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
-import type { AppType } from 'next/dist/shared/lib/utils';
 import superjson from 'superjson';
+
+import { url } from '@/constants';
+import type { AppType } from 'next/dist/shared/lib/utils';
 import type { AppRouter } from '../server/router';
 import '../styles/globals.css';
 
@@ -20,24 +21,8 @@ const MyApp: AppType = ({
 	);
 };
 
-const getBaseUrl = () => {
-	if (typeof window !== 'undefined') {
-		return '';
-	}
-	if (process.browser) return ''; // Browser should use current path
-	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
-	return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
-
 export default withTRPC<AppRouter>({
 	config({ ctx }) {
-		/**
-		 * If you want to use SSR, you need to use the server's full URL
-		 * @link https://trpc.io/docs/ssr
-		 */
-		const url = `${getBaseUrl()}/api/trpc`;
-
 		return {
 			url,
 			transformer: superjson,
