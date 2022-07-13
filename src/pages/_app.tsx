c9ca9e6699a6dc1f95cb1ac18e3919f4f042a70d
akif <akifunal05@gmail.com>
2022-07-14 01:17:27 +0300

    style: themeprovider added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 // src/pages/_app.tsx
 import { withTRPC } from '@trpc/next';
-import type { AppRouter } from '../server/router';
+import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider } from 'next-themes';
 import type { AppType } from 'next/dist/shared/lib/utils';
 import superjson from 'superjson';
-import { SessionProvider } from 'next-auth/react';
+import type { AppRouter } from '../server/router';
 import '../styles/globals.css';
 
 const MyApp: AppType = ({
@@ -12,7 +13,9 @@ const MyApp: AppType = ({
 }) => {
 	return (
 		<SessionProvider session={session}>
-			<Component {...pageProps} />
+			<ThemeProvider enableSystem={true} attribute='class'>
+				<Component {...pageProps} />
+			</ThemeProvider>
 		</SessionProvider>
 	);
 };
