d17b2bc7af7eb37f1fd07f147d6d9b9a5b812b80
akif <akifunal05@gmail.com>
2022-07-14 01:37:48 +0300

    refactor: long paths changed with absolute paths

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,10 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import GithubProvider from 'next-auth/providers/github';
 
 // Prisma adapter for NextAuth, optional and can be removed
+import { prisma } from '@db/client';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { prisma } from '../../../server/db/client';
 
 export const authOptions: NextAuthOptions = {
 	// Configure one or more authentication providers
--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -1,6 +1,5 @@
-// src/pages/api/examples.ts
+import { prisma } from '@db/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '../../server/db/client';
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
 	const examples = await prisma.example.findMany();
