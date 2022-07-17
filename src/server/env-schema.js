c847ae9f43fe1e2d3ff0e32cd2bb27b3ee089175
Akif ÜNAL <akifunal05@gmail.com>
2022-07-17 18:55:23 +0300

    feat(twitch-auth): twitch integration added for authentication (#5)

--- src/server/env-schema.js
+++ src/server/env-schema.js
@@ -8,6 +8,8 @@ const envSchema = z.object({
 	NEXTAUTH_SECRET: z.string(),
 	NEXTAUTH_URL: z.string().url(),
 	NODE_ENV: z.enum(['development', 'test', 'production']),
+	TWITCH_CLIENT_ID: z.string(),
+	TWITCH_CLIENT_SECRET: z.string(),
 });
 
 module.exports.envSchema = envSchema;
